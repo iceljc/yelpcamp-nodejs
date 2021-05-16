ffd006f2cc4d39e32eb9aad001cab8af58a9e6de
iceljc <franklujc@gmail.com>
2021-05-16 14:58:44 -0500

    config start script

--- app.js
+++ app.js
@@ -197,7 +197,8 @@ app.use((err, req, res, next) => {
 
 
 // start server
-app.listen(3000, () => {
-    console.log("Server on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server on port ${port}`);
 })
 
